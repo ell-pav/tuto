51323f60163ea02b7d2269c14764ea38c7af259c
ell-pav <elliot.pavesi.pro@gmail.com>
2026-01-12 16:29:22 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+uv init
+uv add ....
+uv add --dev pytest sphinx sphinx-rtb-theme
+uv sync
+uv run sphinx-quickstart
---
+++ tests/test_valid.py
@@ -0,0 +1,5 @@
+"""fichier contenant les tests"""
+
+def test_true():
+    """premier test valise"""
+    assert True
